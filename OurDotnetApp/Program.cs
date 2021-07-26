4b2119489b6507493892c0bfd618ccfaf8542ed7
Gavin Stark <gavin@gstark.com>
2021-07-26 12:24:45 -0400

    First example C# program

---
+++ OurDotnetApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OurDotnetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit
+            var personName = "Samantha";
+            var score = 95;
+
+            // Explicit
+            string explicityName = "Frank";
+            int price = 42;
+
+            var total = score + price;
+
+            bool isExpensive = true;
+            bool isTuesday = false;
+
+            var sentence = "The quick brown fox jumped over the lazy dog";
+            var howLong = sentence.Length;
+
+            var rightNow = DateTime.Now;
+            var currentMonth = rightNow.Month;
+
+            // This is a name of a variable
+            // and I wan't it to be omg, what is happening
+
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.Write("It is a pleasure to meet you, ");
+            Console.WriteLine(name);
+
+            var greeting = $"It is a pleasure to meet you, {name}";
+            Console.WriteLine(greeting);
+        }
+    }
+}
