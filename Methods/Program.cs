7c71be551832bd1e8d9d790194c233fa897d9ae5
Gavin Stark <gavin@gstark.com>
2021-07-28 11:38:20 -0400

    Makes a DisplayGreeting method

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,9 +4,31 @@ namespace Methods
 {
   class Program
   {
+    static void DisplayGreeting()
+    {
+      Console.WriteLine("--------------------------------");
+      Console.WriteLine("Welcome to Our Employee Database");
+      Console.WriteLine("--------------------------------");
+      Console.WriteLine();
+      Console.WriteLine();
+    }
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      DisplayGreeting();
+
+      Console.Write("What is your name? ");
+      var name = Console.ReadLine();
+
+      Console.Write("What is your department number? ");
+      var department = int.Parse(Console.ReadLine());
+
+      Console.Write("What is your yearly salary (in dollars)? ");
+      var salary = int.Parse(Console.ReadLine());
+
+      var salaryPerMonth = salary / 12;
+      Console.WriteLine($"Hello, {name} you make {salaryPerMonth} a month.");
+
     }
   }
 }
