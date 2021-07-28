346e03ed3f75916d65a0883c27dea4a874e8d9dd
Gavin Stark <gavin@gstark.com>
2021-07-28 09:45:42 -0400

    Algorithm B - using loops for making a deck

--- AllCardsOnDeck/Program.cs
+++ AllCardsOnDeck/Program.cs
@@ -7,12 +7,39 @@ namespace AllCardsOnDeck
   {
     static void Main(string[] args)
     {
+      // Old Algorithm A
+      //
+      // // - Make a new list of strings named `deck`
+      // // - Initialize the list of strings with 52 explicity stated cards from our Example section - Shuffle them with Fisher Yates (paste algo here)
+      // var deck = new List<string>() {
+      //   "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs",
+      //   "Ace of Diamonds", "2 of Diamonds",
+      // };
+
+      // Algorithm B
+      // - Make a list of the 4 suits and call this list `suits`
+      var suits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
+
+      // - Make a list of the 13 ranks and call this list `ranks`
+      var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
       // - Make a new list of strings named `deck`
-      // - Initialize the list of strings with 52 explicity stated cards from our Example section - Shuffle them with Fisher Yates (paste algo here)
-      var deck = new List<string>() {
-        "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs",
-        "Ace of Diamonds", "2 of Diamonds",
-      };
+      var deck = new List<string>();
+
+      // - Make a loop that goes through the list `suits`
+      foreach (var suit in suits)
+      {
+        //   - Make a loop that goes through the list `ranks`
+        foreach (var rank in ranks)
+        {
+          //     - for each rank, make a new string, called "card" of the form $"{rank} of {suit}"
+          var card = $"{rank} of {suit}";
+
+          //     - add that newly formed string to the end of the deck
+          deck.Add(card);
+        }
+      }
+
 
       //   numberOfCards = length of our deck
       var numberOfCards = deck.Count;
