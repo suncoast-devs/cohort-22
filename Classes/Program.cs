5b1202d07bcb0145ee759df6b2c19a5d7281e2a1
Gavin Stark <gavin@gstark.com>
2021-07-29 10:20:40 -0400

    Added a class
    Add more detail paragraph here.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,7 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Methods
 {
+
+  //
+  // class keyword
+  // |
+  // |    Name of class (PascalCase)
+  // |    |
+  // v    v
+  class Employee
+  {
+    // public means "this can be seen outside of the class"
+    // |
+    // |   Type
+    // |   |
+    // |   |      Name of property
+    // |   |      |
+    // |   |      |
+    // |   |      |
+    // v   v      v
+    public string Name;
+    public int Department;
+    public int Salary;
+    public int MonthlySalary;
+
+    // This is a *special* method known as a "constructor"
+    // The constructor is called when we write a line like: `var bob = new Employee(`
+    // The arguments to the method should line up to those below
+    //
+    //              This will become the employee's name
+    //              |               This will become the employee's department
+    //              |               |                  This will become the employee's salary
+    //              |               |                  |              This will become the employee's monthly salary
+    //              |               |                  |              |
+    //              v               v                  v              v
+    public Employee(string newName, int newDepartment, int newSalary, int newMonthlySalary)
+    {
+      // In the constructor we should setup the values for any of the properties.
+      // Here we will *copy* the values given by the arguments to the corresponding property.
+
+      Name = newName;
+      Department = newDepartment;
+      Salary = newSalary;
+      MonthlySalary = newMonthlySalary;
+    }
+  }
+
+
   class Program
   {
     static void DisplayGreeting()
@@ -53,6 +100,24 @@ namespace Methods
 
     static void Main(string[] args)
     {
+
+      var graceHopper = new Employee("Grace Hopper", 100, 240_000, 20_000);
+      Console.WriteLine(graceHopper.Department);
+
+      var elonMusk = new Employee("Elon Musk", 42, 120_000, 10_000);
+      Console.WriteLine(elonMusk.Department);
+
+      var scores = new List<int>() { 42, 100, 55 };
+
+      var employees = new List<Employee>() {
+         graceHopper,
+         elonMusk,
+         new Employee("Gavin Stark", 42, 12_000, 1_000)
+      };
+      // employees.Add(graceHopper);
+      // employees.Add(elonMusk);
+
+      employees.RemoveAt(0);
       DisplayGreeting();
 
       var name = PromptForString("What is your name? ");
