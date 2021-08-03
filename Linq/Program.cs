4ceb63a7065def0e0988f7482aabf89b087a21d0
Gavin Stark <gavin@gstark.com>
2021-08-03 09:55:11 -0400

    Long way to double an array

---
+++ Linq/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linq
+{
+  class Program
+  {
+    static int MultiplyBy2(int value)
+    {
+      return value * 2;
+    }
+
+    static void Main(string[] args)
+    {
+      var scores = new List<int> { 42, 100, 98, 15 };
+
+      // Here is our handy multiply by two
+      // Func<int, int> MultiplyBy2 = value => value * 2;
+
+      // Make a new list to store the results
+      var newScores = new List<int>();
+
+      // Go through each score in the scores list
+      foreach (var score in scores)
+      {
+        // Use the `MultiplyBy2` expression to take score and double it
+        var doubled = MultiplyBy2(score);
+
+        // Add it to our new list
+        newScores.Add(doubled);
+      }
+      Console.WriteLine();
+    }
+  }
+}
