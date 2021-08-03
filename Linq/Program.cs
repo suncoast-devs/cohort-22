6f408b43185052135aa812a4e8d101e242e87850
Gavin Stark <gavin@gstark.com>
2021-08-03 10:04:02 -0400

    Using Linq and Select to turn one list into another

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Linq
 {
@@ -17,18 +18,20 @@ namespace Linq
       // Here is our handy multiply by two
       Func<int, int> MultiplyBy2 = value => value * 2;
 
-      // Make a new list to store the results
-      var newScores = new List<int>();
+      var newScores = scores.Select(MultiplyBy2);
 
-      // Go through each score in the scores list
-      foreach (var score in scores)
-      {
-        // Use the `MultiplyBy2` expression to take score and double it
-        var doubled = MultiplyBy2(score);
+      // // Make a new list to store the results
+      // var newScores = new List<int>();
 
-        // Add it to our new list
-        newScores.Add(doubled);
-      }
+      // // Go through each score in the scores list
+      // foreach (var score in scores)
+      // {
+      //   // Use the `MultiplyBy2` expression to take score and double it
+      //   var doubled = MultiplyBy2(score);
+
+      //   // Add it to our new list
+      //   newScores.Add(doubled);
+      // }
       Console.WriteLine();
     }
   }
