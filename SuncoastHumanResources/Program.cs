14de31d6d6a47aed785b78d17627bfd8319be5ec
Gavin Stark <gavin@gstark.com>
2021-08-04 10:09:39 -0400

    Improves our searching code by using LINQ

--- SuncoastHumanResources/Program.cs
+++ SuncoastHumanResources/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SuncoastHumanResources
 {
@@ -76,21 +77,10 @@ namespace SuncoastHumanResources
         else
         if (choice == "F")
         {
-          // - Create a variable named **`foundEmployee`**
-          Employee foundEmployee = null;
-
           // - Prompt for the name
           var nameToSearchFor = PromptForString("What name are you looking for? ");
 
-          // - Loop through the list to look for a match
-          foreach (var employee in employees)
-          {
-            //   - If we find one, update `foundEmployee`
-            if (employee.Name == nameToSearchFor)
-            {
-              foundEmployee = employee;
-            }
-          }
+          Employee foundEmployee = employees.FirstOrDefault(employee => employee.Name == nameToSearchFor);
 
           // - After the loop, `foundEmployee` is either `null` (not found) or refers to the matching item
           if (foundEmployee == null)
