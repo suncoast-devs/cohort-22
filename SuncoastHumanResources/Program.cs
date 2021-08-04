e95d2a4b9e55cb362ebc297c72a56636925f1e6e
Gavin Stark <gavin@gstark.com>
2021-08-04 09:46:31 -0400

    Initial code and list of employees

--- SuncoastHumanResources/Program.cs
+++ SuncoastHumanResources/Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace SuncoastHumanResources
 {
+  class Employee
+  {
+    public string Name { get; set; }
+    public int Department { get; set; }
+    public int Salary { get; set; }
+    public int MonthlySalary()
+    {
+      return Salary / 12;
+    }
+  }
+
   class Program
   {
+    static void DisplayGreeting()
+    {
+      Console.WriteLine("----------------------------------------");
+      Console.WriteLine("    Welcome to Our Employee Database    ");
+      Console.WriteLine("----------------------------------------");
+      Console.WriteLine();
+      Console.WriteLine();
+    }
+
+    static string PromptForString(string prompt)
+    {
+      Console.Write(prompt);
+      var userInput = Console.ReadLine();
+
+      return userInput;
+    }
+
+    static int PromptForInteger(string prompt)
+    {
+      Console.Write(prompt);
+      int userInput;
+      var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+      if (isThisGoodInput)
+      {
+        return userInput;
+      }
+      else
+      {
+        Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+        return 0;
+      }
+    }
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      var employees = new List<Employee>();
+
+      var employee = new Employee();
+
+      DisplayGreeting();
+
+      employee.Name = PromptForString("What is your name? ");
+      employee.Department = PromptForInteger("What is your department number? ");
+      employee.Salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+
+      Console.WriteLine($"Hello, {employee.Name} you make {employee.MonthlySalary()} dollars per month.");
     }
   }
 }
