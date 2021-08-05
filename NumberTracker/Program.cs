2a769ca3875bdc9f74ada80619a089c8861ff72c
Gavin Stark <gavin@gstark.com>
2021-08-05 10:10:01 -0400

    Basic NumberTracker app

--- NumberTracker/Program.cs
+++ NumberTracker/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NumberTracker
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Number Tracker");
+
+            // - Create an empty list of numbers.
+            var numbers = new List<int>();
+
+            // Controls if we are still running our loop asking for more numbers
+            var isRunning = true;
+
+            // While we are running
+            while (isRunning)
+            {
+                // Show the list of numbers
+                Console.WriteLine("------------------");
+                foreach (var number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+                Console.WriteLine($"Our list has: {numbers.Count()} entries");
+                Console.WriteLine("------------------");
+
+                // Ask for a new number or the word quit to end
+                Console.Write("Enter a number to store, or 'quit' to end: ");
+                var input = Console.ReadLine().ToLower();
+
+                if (input == "quit")
+                {
+                    // If the input is quit, turn off the flag to keep looping
+                    isRunning = false;
+                }
+                else
+                {
+                    // Parse the number and add it to the list of numbers
+                    var number = int.Parse(input);
+                    numbers.Add(number);
+                }
+            }
+        }
     }
-  }
 }
