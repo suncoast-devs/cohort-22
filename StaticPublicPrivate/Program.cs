50dc306fd36b5f3d56d258e2eee4bd3aa4637e58
Gavin Stark <gavin@gstark.com>
2021-08-05 09:50:18 -0400

    Adds examples

--- StaticPublicPrivate/Program.cs
+++ StaticPublicPrivate/Program.cs
@@ -2,10 +2,57 @@
 
 namespace StaticPublicPrivate
 {
+  class Math
+  {
+    static public double Pi()
+    {
+      return 3.14159265;
+    }
+  }
+
+  class Bird
+  {
+    public string Name { get; set; }
+
+
+    // Constructor
+    // IS a bird
+    public Bird()
+    {
+      Squawk();
+    }
+
+    // Is not *A* bird, it is bird class itself!
+    static public bool AreTheyAwesome()
+    {
+      var bird = new Bird();
+      bird.Squawk();
+
+      return true;
+    }
+
+    // Is a bird
+    public void Squawk()
+    {
+      Console.WriteLine($"{Name} says Squawk!");
+      if (Bird.AreTheyAwesome())
+      {
+        Console.WriteLine("AWESOME!");
+      }
+    }
+  }
+
   class Program
   {
     static void Main(string[] args)
     {
+      var louie = new Bird();
+      louie.Squawk();
+
+      Bird.AreTheyAwesome();
+
+      var pizza = 2 * Math.Pi() * 16;
+
       Console.WriteLine("Welcome to C#");
     }
   }
