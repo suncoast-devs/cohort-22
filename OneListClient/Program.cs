45198a5f300d90801b87baf48d25dadb019b9f4c
Gavin Stark <gavin@gstark.com>
2021-08-17 11:41:17 -0400

    Gets one item and *HANDLES* exceptions

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -9,6 +9,31 @@ namespace OneListClient
 {
   class Program
   {
+
+    static async Task GetOneItem(string token, int id)
+    {
+      try
+      {
+        var client = new HttpClient();
+
+        var responseBodyAsStream = await client.GetStreamAsync($"https://one-list-api.herokuapp.com/items/{id}?access_token={token}");
+
+        //                                          Describe the Shape of the data (Object in JSON => Item)
+        //                                                V       
+        var item = await JsonSerializer.DeserializeAsync<Item>(responseBodyAsStream);
+
+        var table = new ConsoleTable("Description", "Created At", "Completed");
+
+        table.AddRow(item.Text, item.CreatedAt, item.CompletedStatus);
+
+        table.Write();
+      }
+      catch (HttpRequestException)
+      {
+        Console.WriteLine("I could not find that item");
+      }
+    }
+
     static async Task ShowAllItems(string token)
     {
       var client = new HttpClient();
@@ -48,13 +73,23 @@ namespace OneListClient
       while (keepGoing)
       {
         Console.Clear();
-        Console.Write("Get (A)ll todo, or (Q)uit: ");
+        Console.Write("Get (A)ll todo, (O)ne Todo, or (Q)uit: ");
         var choice = Console.ReadLine().ToUpper();
 
         switch (choice)
         {
           case "Q":
             keepGoing = false;
+            break;
+
+          case "O":
+            Console.Write("Enter the ID ");
+            var id = int.Parse(Console.ReadLine());
+
+            await GetOneItem(token, id);
+
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
             break;
 
           case "A":
