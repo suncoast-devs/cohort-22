79f5c3a4bbd9402a9b2d9a27338674b81571f0e7
Gavin Stark <gavin@gstark.com>
2021-08-17 11:18:20 -0400

    Using JsonPropertyName *annotation* to be flexible to rename them

--- OneListClient/Item.cs
+++ OneListClient/Item.cs
@@ -1,20 +1,30 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace OneListClient
 {
   public class Item
   {
-    public int id { get; set; }
-    public string text { get; set; }
-    public bool complete { get; set; }
-    public DateTime created_at { get; set; }
-    public DateTime updated_at { get; set; }
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+
+    [JsonPropertyName("complete")]
+    public bool Complete { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("updated_at")]
+    public DateTime UpdatedAt { get; set; }
 
     public string CompletedStatus
     {
       get
       {
-        return complete ? "Completed" : "Not Completed";
+        return Complete ? "Completed" : "Not Completed";
 
         // if (complete)
         // {
