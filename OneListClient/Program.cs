ed530a9deb49a76b43daab732b40dcc168ca232b
Gavin Stark <gavin@gstark.com>
2021-08-17 11:32:02 -0400

    Added a menu
    Should start to look like Jurrassic Park or Rhythms Gonna Get you

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -3,25 +3,70 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using ConsoleTables;
 
 namespace OneListClient
 {
   class Program
   {
-    static async Task Main(string[] args)
+    static async Task ShowAllItems(string token)
     {
       var client = new HttpClient();
 
-      var responseBodyAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=cohort22");
+      var responseBodyAsStream = await client.GetStreamAsync($"https://one-list-api.herokuapp.com/items?access_token={token}");
 
       //                                          Describe the Shape of the data (array in JSON => List, Object in JSON => Item)
       //                                                V        V
       var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseBodyAsStream);
 
+      var table = new ConsoleTable("Description", "Created At", "Completed");
+
       // Back in the world of List/LINQ/C#
       foreach (var item in items)
       {
-        Console.WriteLine($"The task {item.Text} was created on {item.CreatedAt} and has a completion of {item.CompletedStatus}");
+        table.AddRow(item.Text, item.CreatedAt, item.CompletedStatus);
+      }
+
+      table.Write();
+    }
+
+    static async Task Main(string[] args)
+    {
+      var token = "";
+
+      if (args.Length == 0)
+      {
+        Console.Write("What list would you like? ");
+        token = Console.ReadLine();
+      }
+      else
+      {
+        token = args[0];
+      }
+
+      var keepGoing = true;
+      while (keepGoing)
+      {
+        Console.Clear();
+        Console.Write("Get (A)ll todo, or (Q)uit: ");
+        var choice = Console.ReadLine().ToUpper();
+
+        switch (choice)
+        {
+          case "Q":
+            keepGoing = false;
+            break;
+
+          case "A":
+            await ShowAllItems(token);
+
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            break;
+
+          default:
+            break;
+        }
       }
     }
   }
