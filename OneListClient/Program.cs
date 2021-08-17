58881122ebee4cc7bcb5a87c98fec6bd1a363103
Gavin Stark <gavin@gstark.com>
2021-08-17 10:53:45 -0400

    Gets the response from the API but as a simple string (not interpreted)

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OneListClient
 {
   class Program
   {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
       var client = new HttpClient();
+
+      var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=cohort22");
+
+      Console.WriteLine(responseBodyAsString);
     }
   }
 }
