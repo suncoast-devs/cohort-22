c0e66f017908351caa5b58d8b7ff30af161e22c9
Gavin Stark <gavin@gstark.com>
2021-08-18 09:53:55 -0400

    Update Quine

--- Quine/Program.cs
+++ Quine/Program.cs
@@ -1,1 +1,1 @@
-﻿class Q{static void Main(){var s="class Q{{static void Main(){{var s={1}{0}{1};System.Console.Write(s,s,'{1}');}}}}";System.Console.Write(s,s,'"');}}
+﻿class Q { static void Main() { var s = "class Q{{static void Main(){{var s={1}{0}{1};System.Console.Write(s,s,'{1}');}}}}"; System.Console.Write(s, s, '"'); } }
