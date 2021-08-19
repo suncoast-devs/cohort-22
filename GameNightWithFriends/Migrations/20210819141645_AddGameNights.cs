e29600a57b49c5fa38c9a337abbf9e964cff52d7
Gavin Stark <gavin@gstark.com>
2021-08-19 10:36:11 -0400

    Added migrations for GameNight

---
+++ GameNightWithFriends/Migrations/20210819141645_AddGameNights.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace GameNightWithFriends.Migrations
+{
+    public partial class AddGameNights : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameNights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Host = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    When = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    MinimumNumberOfPlayers = table.Column<int>(type: "integer", nullable: false),
+                    MaximumNumberOfPlayers = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameNights", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameNights");
+        }
+    }
+}
