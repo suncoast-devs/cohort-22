9c84b1c99a8c63dea02045b55be7a3e9aa850760
Gavin Stark <gavin@gstark.com>
2021-08-24 09:41:57 -0400

    Initial Commit

---
+++ HelloCSS/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [checker({ typescript: true })],
+})
