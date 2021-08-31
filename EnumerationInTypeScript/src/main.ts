2f5fe2c430378f84853334051421215b9d18b7d9
Gavin Stark <gavin@gstark.com>
2021-08-31 11:19:55 -0400

    map in JS/TS is just like Select in C#

--- EnumerationInTypeScript/src/main.ts
+++ EnumerationInTypeScript/src/main.ts
@@ -3,13 +3,42 @@ import './style.css'
 console.clear()
 
 const colors = ['red', 'green', 'blue']
-const lengths: number[] = []
+const numbers = [10, 20, 30]
+// const arrayOfStringsAndNumbers = [10, 'Gavin', 42]
 
-function logSomeColor(color: string) {
-  const lengthOfColor = color.length
+// const turnsStringIntoLength = function (someString: string): number {
+//   return someString.length
+// }
 
-  lengths.push(lengthOfColor)
-}
+// map is exactly like SELECT from C#
+// const lengths = colors.map(function (color: string): number {
+//   return color.length
+// })
 
-colors.forEach(logSomeColor)
+// C#:  var lengths = colors.Select(color => color.Length)
+// const lengths = colors.map((color: string): number => color.length)
+const lengths = colors.map((color) => color.length)
+
+// array.map(function (element, index, theArrayItself) {
+//  returns SOMETHING
+// }
+
+const uppercased = colors.map(function (color) {
+  return color.toUpperCase()
+})
+
+const doubled = numbers.map(function (someNumber: number): string {
+  return `The number doubled is ${someNumber * 2}`
+})
+
+// const sentences = arrayOfStringsAndNumbers.map(function (someThing) {
+//   if (someThing instanceof string) {
+//     return someThing.length
+//   } else {
+//     return someThing * 2
+//   }
+// })
+
+console.log(doubled)
 console.log(lengths)
+console.log(uppercased)
