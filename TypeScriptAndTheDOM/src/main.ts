658f24b1459a1c8d005b3798d645e4b19a4aa6da
Gavin Stark <gavin@gstark.com>
2021-09-01 11:13:03 -0400

    Resolved the errors TypeScript was warning us about

--- TypeScriptAndTheDOM/src/main.ts
+++ TypeScriptAndTheDOM/src/main.ts
@@ -4,10 +4,18 @@ import './style.css'
 //
 // Because the event tells us the target
 // and the target is the thing we clicked on!
-function handleClickSquare(event) {
+function handleClickSquare(event: MouseEvent) {
   const thingClickedOn = event.target
 
-  thingClickedOn.textContent = 'X'
+  // If the thing clicked on is an LI Element
+  // - This does several things:
+  // - 1. Checks that the target isn't null
+  // - 2. Let's TypeScript know that *inside* this if statement
+  //      the thingClickedOn is an LI element, and thus we can
+  //      change its textContent
+  if (thingClickedOn instanceof HTMLLIElement) {
+    thingClickedOn.textContent = 'X'
+  }
 }
 
 const allSquares = document.querySelectorAll('li')
