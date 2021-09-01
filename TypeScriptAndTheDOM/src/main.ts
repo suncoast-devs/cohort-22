69fb7002043fc9102389b29269815e5b43fe0207
Gavin Stark <gavin@gstark.com>
2021-09-01 10:33:44 -0400

    Moves our code back to the TypeScript file

--- TypeScriptAndTheDOM/src/main.ts
+++ TypeScriptAndTheDOM/src/main.ts
@@ -1,1 +1,11 @@
 import './style.css'
+
+const firstListItem = document.querySelector('li')
+
+function handleClickSquare(event) {
+  const thingClickedOn = event.target
+
+  thingClickedOn.textContent = 'X'
+}
+
+firstListItem?.addEventListener('click', handleClickSquare)
