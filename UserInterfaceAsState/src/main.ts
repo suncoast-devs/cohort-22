e3d4b26da04d9063035872302a994f20cee795a2
Gavin Stark <gavin@gstark.com>
2021-09-02 11:50:57 -0400

    Scoreboard but with UI as state

--- UserInterfaceAsState/src/main.ts
+++ UserInterfaceAsState/src/main.ts
@@ -1,37 +1,90 @@
 import './style.css'
 
-// State
-let counter = 0
-
 const root = document.querySelector('#root')
+
+let teamOneName = 'Team 1'
+let teamOneScore = 0
+
+let teamTwoName = 'Team 2'
+let teamTwoScore = 0
 
 function render() {
   const html = `
-  <p>${counter + 0}</p>
-  <p>${counter + 1}</p>
-  <p>${counter + 2}</p>
-  <p>${counter + 3}</p>
-  <p>${counter + 4}</p>
-  <p>${counter + 5}</p>
-  <p>${counter + 6}</p>
-  <button>Increment This</button>
+  <header>
+    <h1>My Score Board</h1>
+  </header>
+  <main>
+    <section class="team1">
+      <h2>${teamOneName}</h2>
+      <h3>${teamOneScore}</h3>
+      <fieldset>
+        <input type="text" placeholder="Name" value="${teamOneName}" />
+      </fieldset>
+
+      <fieldset>
+        <i class="add fas fa-2x fa-plus-circle"></i>
+        <i class="subtract fas fa-2x fa-minus-circle"></i>
+      </fieldset>
+    </section>
+
+    <section class="team2">
+      <h2>${teamTwoName}</h2>
+      <h3>${teamTwoScore}</h3>
+      <fieldset>
+        <input type="text" placeholder="Name" value="${teamTwoName}" />
+      </fieldset>
+
+      <fieldset>
+        <i class="add fas fa-2x fa-plus-circle"></i>
+        <i class="subtract fas fa-2x fa-minus-circle"></i>
+      </fieldset>
+    </section>
+  </main>
   `
 
   if (root) {
     root.innerHTML = html
   }
 
-  // After we create the HTML we can
-  // now setup our listener for clicks
-  const button = document.querySelector('button')
-
-  function handleButtonClick() {
-    counter++
-
+  document.querySelector('.team1 .add')?.addEventListener('click', function () {
+    teamOneScore++
     render()
-  }
+  })
 
-  button?.addEventListener('click', handleButtonClick)
+  document
+    .querySelector('.team1 .subtract')
+    ?.addEventListener('click', function () {
+      teamOneScore--
+      render()
+    })
+
+  document
+    .querySelector('.team1 input')
+    ?.addEventListener('input', function (event) {
+      const target = event.target as HTMLInputElement
+
+      teamOneName = target?.value
+      render()
+    })
+
+  document.querySelector('.team2 .add')?.addEventListener('click', function () {
+    teamTwoScore++
+    render()
+  })
+  document
+    .querySelector('.team2 .subtract')
+    ?.addEventListener('click', function () {
+      teamTwoScore--
+      render()
+    })
+  document
+    .querySelector('.team2 input')
+    ?.addEventListener('input', function (event) {
+      const target = event.target as HTMLInputElement
+
+      teamTwoName = target?.value
+      render()
+    })
 }
 
 render()
