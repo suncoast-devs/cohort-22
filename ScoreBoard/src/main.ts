acf259eed9c4a3e54c6ba3b5347a48966820b833
Gavin Stark <gavin@gstark.com>
2021-09-02 10:40:29 -0400

    Added code for team2 and improved the selectors for team1

--- ScoreBoard/src/main.ts
+++ ScoreBoard/src/main.ts
@@ -1,12 +1,19 @@
 import './style.css'
 
+console.clear()
+
 // Find all the elements we are going to work with
 // either for adding event listeners *OR* updating
 
-const teamOnePlusButton = document.querySelector('i')
-const teamOneMinusButton = document.querySelector('i.subtract')
-const teamOneScoreText = document.querySelector('h3')
+const teamOnePlusButton = document.querySelector('.team1 i.add')
+const teamOneMinusButton = document.querySelector('.team1 i.subtract')
+const teamOneScoreText = document.querySelector('.team1 h3')
 let teamOneScore = 0
+
+const teamTwoPlusButton = document.querySelector('.team2 i.add')
+const teamTwoMinusButton = document.querySelector('.team2 i.subtract')
+const teamTwoScoreText = document.querySelector('.team2 h3')
+let teamTwoScore = 0
 
 function handleClickOnTeamOnePlusButton() {
   teamOneScore++
@@ -34,3 +41,30 @@ function handleClickOnTeamOneMinusButton() {
 }
 
 teamOneMinusButton?.addEventListener('click', handleClickOnTeamOneMinusButton)
+
+function handleClickOnTeamTwoPlusButton() {
+  teamTwoScore++
+
+  if (teamTwoScoreText) {
+    teamTwoScoreText.textContent = `${teamTwoScore}`
+  }
+}
+
+teamTwoPlusButton?.addEventListener('click', handleClickOnTeamTwoPlusButton)
+
+function handleClickOnTeamTwoMinusButton() {
+  // Guard clause here to protect the code below...
+  if (teamTwoScore === 0) {
+    console.debug('Ooops, user tried to go less than 0')
+    // GUARD, early return
+    return
+  }
+
+  teamTwoScore--
+
+  if (teamTwoScoreText) {
+    teamTwoScoreText.textContent = `${teamTwoScore}`
+  }
+}
+
+teamTwoMinusButton?.addEventListener('click', handleClickOnTeamTwoMinusButton)
