e7c3774ab5142db206c2a6022761fd2cd4ef0a92
Gavin Stark <gavin@gstark.com>
2021-09-02 10:06:30 -0400

    Can handle clicking on team one's buttons

--- ScoreBoard/src/main.ts
+++ ScoreBoard/src/main.ts
@@ -6,3 +6,15 @@ import './style.css'
 const teamOnePlusButton = document.querySelector('i')
 const teamOneMinusButton = document.querySelector('i.subtract')
 const teamOneScore = document.querySelector('h3')
+
+function handleClickOnTeamOnePlusButton() {
+  console.log('I clicked on team one plus button')
+}
+
+teamOnePlusButton?.addEventListener('click', handleClickOnTeamOnePlusButton)
+
+function handleClicikOnTeamOneMinusButton() {
+  console.log('I clicked on team one minus button')
+}
+
+teamOneMinusButton?.addEventListener('click', handleClicikOnTeamOneMinusButton0)
