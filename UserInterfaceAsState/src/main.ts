c1586394ef9c4602fa0df47dd83e9e3de5528dd5
Gavin Stark <gavin@gstark.com>
2021-09-02 11:36:40 -0400

    Simple example of repainting the page

--- UserInterfaceAsState/src/main.ts
+++ UserInterfaceAsState/src/main.ts
@@ -1,1 +1,33 @@
 import './style.css'
+
+// State
+let counter = 0
+
+function render() {
+  const html = `
+  <p>${counter + 0}</p>
+  <p>${counter + 1}</p>
+  <p>${counter + 2}</p>
+  <p>${counter + 3}</p>
+  <p>${counter + 4}</p>
+  <p>${counter + 5}</p>
+  <p>${counter + 6}</p>
+  <button>Increment This</button>
+  `
+
+  document.body.innerHTML = html
+
+  // After we create the HTML we can
+  // now setup our listener for clicks
+  const button = document.querySelector('button')
+
+  function handleButtonClick() {
+    counter++
+
+    render()
+  }
+
+  button?.addEventListener('click', handleButtonClick)
+}
+
+render()
