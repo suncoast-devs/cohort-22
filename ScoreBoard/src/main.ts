a962b7a04833595606c684284f9454465f89cfd1
Gavin Stark <gavin@gstark.com>
2021-09-02 10:01:47 -0400

    Found the three elements that relate to team one's score

--- ScoreBoard/src/main.ts
+++ ScoreBoard/src/main.ts
@@ -1,1 +1,8 @@
 import './style.css'
+
+// Find all the elements we are going to work with
+// either for adding event listeners *OR* updating
+
+const teamOnePlusButton = document.querySelector('i')
+const teamOneMinusButton = document.querySelector('i.subtract')
+const teamOneScore = document.querySelector('h3')
