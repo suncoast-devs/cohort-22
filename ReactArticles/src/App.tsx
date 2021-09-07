1fd077225aaf21e91c174efc2d0b9db10dda779a
Gavin Stark <gavin@gstark.com>
2021-09-07 11:27:41 -0400

    Initial JSX and CSS

--- ReactArticles/src/App.tsx
+++ ReactArticles/src/App.tsx
@@ -1,5 +1,32 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div className="all-main-content">
+      <main>
+        <article className="intro-article">
+          <h2 className="article-title">SDG Announces Hackathon!</h2>
+          <p>
+            SDG announces the 2020 Summer Hackathon. Join us for an exciting
+            weekend
+          </p>
+          <a className="read-more" href="#here">
+            read more about SDG Announces Hackathon!
+          </a>
+        </article>
+
+        <article className="intro-article">
+          <h2 className="article-title">
+            Student Graduation is Right Around the Corner
+          </h2>
+          <p>
+            Our next cohort of students will be graduating in just over a week.
+          </p>
+          <a className="read-more" href="#here">
+            read more about Student Graduation is Right Around the Corner
+          </a>
+        </article>
+      </main>
+    </div>
+  )
 }
