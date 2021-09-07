1a32a88773378361e7a53a612474ff223cfa7855
Gavin Stark <gavin@gstark.com>
2021-09-07 11:09:52 -0400

    Initial Commit

---
+++ ReactArticles/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import checker from 'vite-plugin-checker'
+import { EsLinter, linterPlugin, TypeScriptLinter } from 'vite-plugin-linter'
+
+// https://vitejs.dev/config/
+export default defineConfig((configEnv) => ({
+  plugins: [
+    reactRefresh(),
+    checker({ typescript: true }),
+    linterPlugin({
+      include: ['./src/**/*.ts', './src/**/*.tsx'],
+      linters: [
+        new EsLinter({
+          configEnv: configEnv,
+          serveOptions: { formatter: 'visualstudio' },
+        }),
+        new TypeScriptLinter(),
+      ],
+    }),
+  ],
+}))
