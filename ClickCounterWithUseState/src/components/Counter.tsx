ab9a43af684641bebf6ab6852ffa4ca212f60b19
Gavin Stark <gavin@gstark.com>
2021-09-08 11:33:35 -0400

    Simplify the click counting code

--- ClickCounterWithUseState/src/components/Counter.tsx
+++ ClickCounterWithUseState/src/components/Counter.tsx
@@ -8,12 +8,8 @@ export function Counter() {
 
   const [counter, setCounter] = useState(0)
 
-  function handleClickCounter(event: React.MouseEvent) {
-    event.preventDefault()
-
-    const newCounter = counter + 1
-
-    setCounter(newCounter)
+  function handleClickCounter() {
+    setCounter(counter + 1)
   }
 
   return (
