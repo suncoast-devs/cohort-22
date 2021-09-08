7969108debb27676b0b9eaca7511a30fbc0f9f65
Gavin Stark <gavin@gstark.com>
2021-09-08 15:15:39 -0400

    Step 1: Static HTML + CSS

--- ReactSliderDemo/src/App.tsx
+++ ReactSliderDemo/src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <main>
+      <section className="gauge">
+        <figure>
+          <i className="fas fa-circle-notch" />
+          <span>78&deg;</span>
+        </figure>
+        <input type="range" min="50" max="120" value="78" />
+      </section>
+    </main>
+  )
 }
