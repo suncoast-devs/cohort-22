1782fc9067e37229712a7f37b3aedf5e49e9855e
Gavin Stark <gavin@gstark.com>
2021-09-08 10:34:46 -0400

    Added assets
    NOTE: Not deployable since we have a problem with our template

--- ReactArticles/src/App.tsx
+++ ReactArticles/src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { NewsArticle } from './components/NewsArticle'
 import articles from './articles.json'
+import logo from './images/banner.png'
 
 export function App() {
   /* code here */
 
   // const yelling = words.map(word => word.toUpperCase())
 
+  // console.log(logo)
   const newsArticlesFromData = articles.map((article) => (
     <NewsArticle key={article.id} title={article.title} body={article.body} />
   ))
 
   return (
     <div className="all-main-content">
+      <img width={250} src={logo} alt="SDG" />
       <main>{newsArticlesFromData}</main>
     </div>
   )
