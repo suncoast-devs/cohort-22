a466b0105912a0c93ff4e10d23c465f8a956e6d5
Gavin Stark <gavin@gstark.com>
2021-09-09 11:51:34 -0400

    Uses a two dimensional array

--- ReactTicTacToe/src/App.tsx
+++ ReactTicTacToe/src/App.tsx
@@ -62,15 +62,16 @@ export function App() {
         {header} - {game.id} <button onClick={handleNewGame}>New</button>
       </h1>
       <ul>
-        <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
-        <li onClick={() => handleClickCell(0, 1)}>{game.board[0][1]}</li>
-        <li onClick={() => handleClickCell(0, 2)}>{game.board[0][2]}</li>
-        <li onClick={() => handleClickCell(1, 0)}>{game.board[1][0]}</li>
-        <li onClick={() => handleClickCell(1, 1)}>{game.board[1][1]}</li>
-        <li onClick={() => handleClickCell(1, 2)}>{game.board[1][2]}</li>
-        <li onClick={() => handleClickCell(2, 0)}>{game.board[2][0]}</li>
-        <li onClick={() => handleClickCell(2, 1)}>{game.board[2][1]}</li>
-        <li onClick={() => handleClickCell(2, 2)}>{game.board[2][2]}</li>
+        {game.board.map((row, rowIndex) =>
+          row.map((column, columnIndex) => (
+            <li
+              key={columnIndex}
+              onClick={() => handleClickCell(rowIndex, columnIndex)}
+            >
+              {game.board[rowIndex][columnIndex]}
+            </li>
+          ))
+        )}
       </ul>
     </div>
   )
