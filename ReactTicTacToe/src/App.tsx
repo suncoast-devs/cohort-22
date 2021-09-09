9b3ea317d5dd8b65a0b6d0e973ef679ba8693ce0
Gavin Stark <gavin@gstark.com>
2021-09-09 10:58:39 -0400

    Step 2: Manual state and then mess around with it

--- ReactTicTacToe/src/App.tsx
+++ ReactTicTacToe/src/App.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
+  const [game, setGame] = useState({
+    board: [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+    ],
+    id: null,
+    winner: null,
+  })
+
   return (
     <div>
       <h1>
         Tic Tac Toe - <button>New</button>
       </h1>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>{game.board[0][0]}</li>
+        <li>{game.board[0][1]}</li>
+        <li>{game.board[0][2]}</li>
+        <li>{game.board[1][0]}</li>
+        <li>{game.board[1][1]}</li>
+        <li>{game.board[1][2]}</li>
+        <li>{game.board[2][0]}</li>
+        <li>{game.board[2][1]}</li>
+        <li>{game.board[2][2]}</li>
       </ul>
     </div>
   )
