d70fc512401bc4755a12691989b3bac30b42a23c
Gavin Stark <gavin@gstark.com>
2021-09-09 10:51:39 -0400

    Step 1: static implementation

--- ReactTicTacToe/src/App.tsx
+++ ReactTicTacToe/src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <h1>
+        Tic Tac Toe - <button>New</button>
+      </h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
 }
