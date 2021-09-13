6f77fa181106093403d46ee15fe664c3bdef44b6
Gavin Stark <gavin@gstark.com>
2021-09-13 11:01:21 -0400

    Static HTML implementation

---
+++ MineSweeper/README.md
@@ -0,0 +1,7 @@
+React/MineSweeper questions
+
+- Right and Left click for flags
+- Two dimensional arrays
+- How to customize the display of squares/bombs/flags
+- Updating state, etc.
+- localStorage
--- MineSweeper/src/App.tsx
+++ MineSweeper/src/App.tsx
@@ -1,5 +1,88 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <main>
+      <h1>Mine Sweeper</h1>
+      <h2>
+        <button>New Easy Game</button>
+        <button>New Intermediate Game</button>
+        <button>New Difficult Game</button>
+      </h2>
+      <h3>Mines: 100</h3>
+      <h3>Game #: 200</h3>
+      <h3></h3>
+
+      <ul className="difficulty-0">
+        <li className="cell-bomb">
+          <i className="fa fa-bomb" />
+        </li>
+        <li className="cell-flag">
+          <i className="fa fa-flag" />
+        </li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+        <li>_</li>
+      </ul>
+    </main>
+  )
 }
