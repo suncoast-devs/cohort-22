576e90ede1ad677da90892fb0d8443e325d32e8a
Gavin Stark <gavin@gstark.com>
2021-09-13 10:37:30 -0400

    Added rule to ignore unsued arguments that start with an underscore

--- ReactTicTacToe/.eslintrc.js
+++ ReactTicTacToe/.eslintrc.js
@@ -33,5 +33,7 @@ module.exports = {
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
+    // Allow arguments with an underscore to be ignored if they are unused
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
 }
