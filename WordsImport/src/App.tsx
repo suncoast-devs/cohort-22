7bea7da0123aa1ed891242444ec0693da75de356
Gavin Stark <gavin@gstark.com>
2021-09-13 15:29:58 -0400

    Clicking letters and storing state

--- WordsImport/src/App.tsx
+++ WordsImport/src/App.tsx
@@ -1,5 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import words from './words.json'
+
+const ALPHABET = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+]
 
 export function App() {
-  return <div>Hello, World </div>
+  const [guessedLetters, setGuessedLetters] = useState('')
+  const secretWord = words[0]
+
+  console.log(secretWord)
+
+  function clickOnLetter(letter: string) {
+    // Make a new state USING the old state plus the information
+    const newValueForGuessedLetters = `${guessedLetters}${letter}`
+
+    setGuessedLetters(newValueForGuessedLetters)
+  }
+
+  return (
+    <div>
+      <div>Your guessed letters are: {guessedLetters}</div>
+      {ALPHABET.map(function (letter) {
+        return (
+          <button
+            key={letter}
+            onClick={function () {
+              clickOnLetter(letter)
+            }}
+            disabled={guessedLetters.includes(letter)}
+          >
+            {letter}
+          </button>
+        )
+      })}
+    </div>
+  )
 }
