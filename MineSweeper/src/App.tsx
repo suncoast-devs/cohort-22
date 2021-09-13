969f4bbccb357794466a61d97a4d49a2787b7bba
Gavin Stark <gavin@gstark.com>
2021-09-13 11:06:51 -0400

    Game board from state

--- MineSweeper/src/App.tsx
+++ MineSweeper/src/App.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
+  const [game, setGame] = useState({
+    id: 1,
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+    state: 'new',
+    mines: 10,
+  })
+
   return (
     <main>
       <h1>Mine Sweeper</h1>
@@ -9,79 +25,16 @@ export function App() {
         <button>New Intermediate Game</button>
         <button>New Difficult Game</button>
       </h2>
-      <h3>Mines: 100</h3>
-      <h3>Game #: 200</h3>
+      <h3>Mines: {game.mines}</h3>
+      <h3>Game #: {game.id}</h3>
       <h3></h3>
 
       <ul className="difficulty-0">
-        <li className="cell-bomb">
-          <i className="fa fa-bomb" />
-        </li>
-        <li className="cell-flag">
-          <i className="fa fa-flag" />
-        </li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
-        <li>_</li>
+        {game.board.map(function (gameRow) {
+          return gameRow.map(function (square, squareIndex) {
+            return <li key={squareIndex}>{square}</li>
+          })
+        })}
       </ul>
     </main>
   )
