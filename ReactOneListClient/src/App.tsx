c6915e08c5af0e74d0d19ae4789fb651d89d58c2
Gavin Stark <gavin@gstark.com>
2021-09-14 10:17:48 -0400

    using useEffect with an empty dependency array in order to load data for our state ONCE at startup

--- ReactOneListClient/src/App.tsx
+++ ReactOneListClient/src/App.tsx
@@ -1,23 +1,28 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import logo from './images/sdg-logo.png'
 
 export function App() {
-  const [todoItems, setTodoItems] = useState([
-    { id: 1, text: 'Do a thing', complete: false },
-    { id: 2, text: 'Do something else', complete: false },
-    { id: 3, text: 'Do a third thing', complete: false },
-    { id: 4, text: 'Remind me about the important thing', complete: false },
-    {
-      id: 5,
-      text: 'The important things are the important things',
-      complete: false,
-    },
-    {
-      id: 6,
-      text: 'WOW',
-      complete: true,
-    },
-  ])
+  const [todoItems, setTodoItems] = useState([])
+
+  // useEffect has a non-async function
+  useEffect(function () {
+    //
+    //
+    // Our async function inside!
+    async function fetchListOfItems() {
+      const response = await axios.get(
+        'https://one-list-api.herokuapp.com/items?access_token=cohort22'
+      )
+
+      if (response.status === 200) {
+        setTodoItems(response.data)
+      }
+    }
+    //
+    //
+    fetchListOfItems()
+  }, [])
 
   return (
     <div className="app">
