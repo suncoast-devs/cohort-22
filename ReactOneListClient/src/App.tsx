f213d487aee26b58d26dc0f685b103adfd4b0e71
Gavin Stark <gavin@gstark.com>
2021-09-15 09:56:30 -0400

    Refactor the "list" portion into its own component

--- ReactOneListClient/src/App.tsx
+++ ReactOneListClient/src/App.tsx
@@ -1,7 +1,6 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import logo from './images/sdg-logo.png'
-import { TodoItem } from './components/TodoItem'
+import { TodoList } from './TodoList'
 
 export type TodoItemType = {
   id: number
@@ -12,80 +11,13 @@ export type TodoItemType = {
 }
 
 export function App() {
-  const [todoItems, setTodoItems] = useState<TodoItemType[]>([])
-  const [newTodoText, setNewTodoText] = useState('')
-
-  function loadAllTheItems() {
-    //
-    //
-    // Our async function inside!
-    async function fetchListOfItems() {
-      const response = await axios.get(
-        'https://one-list-api.herokuapp.com/items?access_token=cohort22'
-      )
-
-      if (response.status === 200) {
-        setTodoItems(response.data)
-      }
-    }
-    //
-    //
-    fetchListOfItems()
-  }
-
-  // useEffect has a non-async function
-  useEffect(loadAllTheItems, [])
-
-  async function handleCreateNewTodoItem() {
-    const body = {
-      item: { text: newTodoText },
-    }
-
-    const response = await axios.post(
-      'https://one-list-api.herokuapp.com/items?access_token=cohort22',
-      body
-    )
-
-    if (response.status === 201) {
-      loadAllTheItems()
-    }
-  }
-
   return (
     <div className="app">
       <header>
         <h1>One List</h1>
       </header>
       <main>
-        <ul>
-          {todoItems.map(function (todoItem) {
-            return (
-              <TodoItem
-                key={todoItem.id}
-                todoItem={todoItem}
-                reloadItems={loadAllTheItems}
-              />
-            )
-          })}
-        </ul>
-        <form
-          onSubmit={function (event) {
-            // Please form, don't do your usual behavior
-            // *I* will tell you what to do
-            event.preventDefault()
-
-            handleCreateNewTodoItem()
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Whats up?"
-            value={newTodoText}
-            onChange={function (event) {
-              setNewTodoText(event.target.value)
-            }}
-          />
-        </form>
+        <TodoList />
       </main>
       <footer>
         <p>
---
+++ ReactOneListClient/src/TodoList.tsx
@@ -0,0 +1,79 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { TodoItem } from './components/TodoItem'
+import { TodoItemType } from './App'
+
+export function TodoList() {
+  const [todoItems, setTodoItems] = useState<TodoItemType[]>([])
+  const [newTodoText, setNewTodoText] = useState('')
+
+  function loadAllTheItems() {
+    //
+    //
+    // Our async function inside!
+    async function fetchListOfItems() {
+      const response = await axios.get(
+        'https://one-list-api.herokuapp.com/items?access_token=cohort22'
+      )
+
+      if (response.status === 200) {
+        setTodoItems(response.data)
+      }
+    }
+    //
+    //
+    fetchListOfItems()
+  }
+
+  // useEffect has a non-async function
+  useEffect(loadAllTheItems, [])
+
+  async function handleCreateNewTodoItem() {
+    const body = {
+      item: { text: newTodoText },
+    }
+
+    const response = await axios.post(
+      'https://one-list-api.herokuapp.com/items?access_token=cohort22',
+      body
+    )
+
+    if (response.status === 201) {
+      loadAllTheItems()
+    }
+  }
+
+  return (
+    <>
+      <ul>
+        {todoItems.map(function (todoItem) {
+          return (
+            <TodoItem
+              key={todoItem.id}
+              todoItem={todoItem}
+              reloadItems={loadAllTheItems}
+            />
+          )
+        })}
+      </ul>
+      <form
+        onSubmit={function (event) {
+          // Please form, don't do your usual behavior
+          // *I* will tell you what to do
+          event.preventDefault()
+
+          handleCreateNewTodoItem()
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Whats up?"
+          value={newTodoText}
+          onChange={function (event) {
+            setNewTodoText(event.target.value)
+          }}
+        />
+      </form>
+    </>
+  )
+}
