22ad908afb3e6a5ccaebc4145a99fc9c233a0992
Gavin Stark <gavin@gstark.com>
2021-09-15 11:44:19 -0400

    Discusses useEffect with a return that is a teardown

--- CryptoTracker/src/App.tsx
+++ CryptoTracker/src/App.tsx
@@ -21,8 +21,29 @@ export function App() {
     loadCoins()
   })
 
+  useEffect(function () {
+    // If useEffect sets up something that consumes
+    // a resource, like an interval identifier
+    const timerInterval = setInterval(function () {
+      console.log('Calling my interval')
+    }, 1000)
+
+    // ... then we near a teardown function to undo
+    //     that resource
+    function teardown() {
+      console.log('clearing the interval')
+      clearInterval(timerInterval)
+    }
+
+    // ... and we return that function!
+    return teardown
+  }, [])
+
+  const header = `There are ${coins.length} coins`
+
   return (
     <div>
+      <p>{header}</p>
       {coins.map((coin) => (
         <p key={coin.name}>{coin.name}</p>
       ))}{' '}
