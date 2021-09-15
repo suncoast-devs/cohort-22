d547d0963ae97538f819d2c6a4dfca17c8c65caf
Gavin Stark <gavin@gstark.com>
2021-09-15 11:33:24 -0400

    Basic crypto coin app with loading data

---
+++ CryptoTracker/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+type Coin = {
+  name: string
+}
+
+export function App() {
+  const [coins, setCoins] = useState<Coin[]>([])
+
+  useEffect(function () {
+    async function loadCoins() {
+      const response = await fetch('https://api.coincap.io/v2/assets')
+
+      if (response.ok) {
+        const { data } = await response.json()
+
+        setCoins(data)
+      }
+    }
+
+    loadCoins()
+  })
+
+  return (
+    <div>
+      {coins.map((coin) => (
+        <p>{coin.name}</p>
+      ))}{' '}
+    </div>
+  )
+}
