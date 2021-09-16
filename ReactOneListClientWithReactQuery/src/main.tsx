bfe8629450a51ab96f697c02dddaa5deaa564f37
Gavin Stark <gavin@gstark.com>
2021-09-16 09:51:04 -0400

    Added and configured react query

--- ReactOneListClientWithReactQuery/src/main.tsx
+++ ReactOneListClientWithReactQuery/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.scss'
 import { App } from './App'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
