532a3e42dccf7836c95e136478757a28ce6cbb46
Gavin Stark <gavin@gstark.com>
2021-09-20 11:17:46 -0400

    Loads a list of restaurants from the api and shows them on the restaurants page

--- TacoTuesday/ClientApp/src/pages/Restaurants.tsx
+++ TacoTuesday/ClientApp/src/pages/Restaurants.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+
 import tacoTuesday from '../images/taco-tuesday.svg'
 import map from '../images/map.png'
-import { CSSStarsProperties } from '../types'
+import { CSSStarsProperties, RestaurantType } from '../types'
 
 export function Restaurants() {
+  const { data: restaurants = [] } = useQuery<RestaurantType[]>(
+    'restaurants',
+    async function () {
+      const response = await fetch('/api/restaurants')
+
+      // Do not await here... We want to return the promise
+      return response.json()
+    }
+  )
+
   return (
     <main className="home">
       <h1>
@@ -18,30 +30,22 @@ export function Restaurants() {
       </section>
 
       <ul className="results">
-        <li>
-          <h2>Loli&apos;s Mexican Cravings</h2>
-          <p>
-            <span
-              className="stars"
-              style={{ '--rating': 4.7 } as CSSStarsProperties}
-              aria-label="Star rating of this location is 4.7 out of 5."
-            ></span>
-            (2,188)
-          </p>
-          <address>8005 Benjamin Rd, Tampa, FL 33634</address>
-        </li>
-        <li>
-          <h2>La Hacienda Mexicana</h2>
-          <p>
-            <span
-              className="stars"
-              style={{ '--rating': 2.3 } as CSSStarsProperties}
-              aria-label="Star rating of this location is 2.3 out of 5."
-            ></span>
-            (245)
-          </p>
-          <address>5537 Sheldon Rd, Tampa, FL 33615</address>
-        </li>
+        {restaurants.map(function (restaurant) {
+          return (
+            <li key={restaurant.id}>
+              <h2>{restaurant.name}</h2>
+              <p>
+                <span
+                  className="stars"
+                  style={{ '--rating': 4.7 } as CSSStarsProperties}
+                  aria-label="Star rating of this location is 4.7 out of 5."
+                ></span>
+                (2,188)
+              </p>
+              <address>{restaurant.address}</address>
+            </li>
+          )
+        })}
       </ul>
     </main>
   )
--- TacoTuesday/ClientApp/src/types.ts
+++ TacoTuesday/ClientApp/src/types.ts
@@ -3,3 +3,11 @@ import { CSSProperties } from 'react'
 export interface CSSStarsProperties extends CSSProperties {
   '--rating': number
 }
+
+export type RestaurantType = {
+  id: number
+  name: string
+  description: string
+  address: string
+  telephone: string
+}
