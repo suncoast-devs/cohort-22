e69f31fb478efdfc4fab5c9c08ac12d5b447147d
Gavin Stark <gavin@gstark.com>
2021-09-27 10:09:01 -0400

    Added a controller that allows users to be CREATED (and only CREATED, not shown, etc.)

---
+++ TacoTuesday/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TacoTuesday.Models;
+
+namespace TacoTuesday.Controllers
+{
+    // All of these routes will be at the base URL:     /api/Users
+    // That is what "api/[controller]" means below. It uses the name of the controller
+    // in this case UsersController to determine the URL
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // This is the variable you use to have access to your database
+        private readonly DatabaseContext _context;
+
+        // Constructor that recives a reference to your database context
+        // and stores it in _context for you to use in your API methods
+        public UsersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Users
+        //
+        // Creates a new user in the database.
+        //
+        // The `body` of the request is parsed and then made available to us as a User
+        // variable named user. The controller matches the keys of the JSON object the client
+        // supplies to the names of the attributes of our User POCO class. This represents the
+        // new values for the record.
+        //
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            // Indicate to the database context we want to add this new record
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            // Return a response that indicates the object was created (status code `201`) and some additional
+            // headers with details of the newly created object.
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+    }
+}
